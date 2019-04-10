6e9d6a42c0efa5f3c9da67d115127db65d50f8be
StashBank <opavlovskyi@gmail.com>
2019-04-10 16:54:51 +0300

    transaction model

--- src/app/transactions/models/transaction.ts
+++ src/app/transactions/models/transaction.ts
@@ -1,2 +1,16 @@
+import { TransactionType } from '../enums/transaction-type.enum';
+import { TransactionStatus } from '../enums/transaction-status.enum';
+import { Store } from '../../stores/models/store';
+import { Customer } from '../../customers/models/customer';
+import { Order } from '../../orders/models/order';
+
 export class Transaction {
+  id: string;
+  type: TransactionType;
+  status: TransactionStatus;
+  from: Store;
+  to: Store | Customer;
+  order: Order;
+  date: Date;
+  qty: number;
 }
