6ba6ee53a5bdcd117378c14d15c00561f47d953b
StashBank <opavlovskyi@gmail.com>
2019-04-10 16:15:08 +0300

    Store model

--- src/app/stores/models/store.ts
+++ src/app/stores/models/store.ts
@@ -1,2 +1,12 @@
+import { StoreType } from '../enums/store-type.enum';
+import { Stock } from '../../stocks/models/stock.model';
+
 export class Store {
+  id: string;
+  type: StoreType;
+  name: string;
+  code: string;
+  address: string;
+  phone: string;
+  stocks: Array<Stock>;
 }
