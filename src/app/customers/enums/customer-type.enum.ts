631ceb69f7ad368ba2b06caa140e39157e3345c6
StashBank <opavlovskyi@gmail.com>
2019-04-10 16:08:12 +0300

    Customer model

--- src/app/customers/enums/customer-type.enum.ts
+++ src/app/customers/enums/customer-type.enum.ts
@@ -1,2 +1,4 @@
 export enum CustomerType {
+  Person,
+  Company
 }
--- src/app/customers/models/customer.ts
+++ src/app/customers/models/customer.ts
@@ -1,2 +1,9 @@
+import { CustomerType } from '../enums/customer-type.enum';
 export class Customer {
+  id: string;
+  name: string;
+  type: CustomerType;
+  phone: string;
+  address: string;
+  extras: string;
 }
