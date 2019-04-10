feaaaffc1a9c5bb168626d1c5d9b8662fd78bdd9
StashBank <opavlovskyi@gmail.com>
2019-04-10 16:28:58 +0300

    order model

--- src/app/orders/models/order.ts
+++ src/app/orders/models/order.ts
@@ -1,2 +1,14 @@
+import { OrderStatus } from '../enums/order-status.enum';
+import { Customer } from '../../customers/models/customer';
+import { Item } from '../../items/models/item';
+import { Store } from '../../stores/models/store';
+
 export class Order {
+  id: string;
+  number: string;
+  date: Date;
+  customer: Customer;
+  item: Item;
+  store: Store;
+  qty: number;
 }
