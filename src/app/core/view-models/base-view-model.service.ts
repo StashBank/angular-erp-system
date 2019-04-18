71907af53e6272222f598e0edf5810b5129e1894
StashBank <opavlovskyi@gmail.com>
2019-04-19 00:09:04 +0300

    merge

--- src/app/core/view-models/base-view-model.service.ts
+++ src/app/core/view-models/base-view-model.service.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectorRef, Injector, Type } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { MatDialog } from '@angular/material';
+import { LookupDialogComponent, DialogData } from '../lookup-dialog/lookup-dialog.component';
 
 export abstract class BaseViewModel {
 
@@ -18,6 +21,7 @@ export abstract class BaseViewModel {
   protected route: ActivatedRoute;
   protected router: Router;
   protected location: Location;
+  protected dialog: MatDialog;
 
   private mobileQueryListener: (ev: MediaQueryListEvent) => void;
 
@@ -29,7 +33,7 @@ export abstract class BaseViewModel {
 
   constructor(
     private injector: Injector,
-    ) {
+  ) {
     this.setUpDeps();
 
     const changeDetectorRef = this.injector.get(ChangeDetectorRef);
@@ -49,11 +53,27 @@ export abstract class BaseViewModel {
     this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
   }
 
+  public openLookup(collectionName: string, formControlName: string, displayedColumns: Array<{ path: string, title: string }>) {
+    const dialogRef = this.dialog.open(LookupDialogComponent, {
+      width: this.isMobile ? '320px' : '800px',
+      data: {
+        collectionName,
+        displayedColumns
+      } as DialogData
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result && formControlName) {
+        this.form.get(formControlName).setValue(result.name || result.code || result.number || result);
+      }
+    });
+  }
+
   private setUpDeps() {
     this.translate = this.injector.get(TranslateService);
     this.formBuilder = this.injector.get(FormBuilder);
     this.route = this.injector.get(ActivatedRoute);
     this.router = this.injector.get(Router);
     this.location = this.injector.get(Location);
+    this.dialog = this.injector.get(MatDialog);
   }
 }
